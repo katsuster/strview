25c1dfcda5e5a3b05cd17f869f0bd030f18655af
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2017-01-31 00:40:00 +0900

    Fix javadoc.

--- view/src/net/katsuster/strview/media/mkv/MKVHeaderBlock.java
+++ view/src/net/katsuster/strview/media/mkv/MKVHeaderBlock.java
@@ -74,14 +74,6 @@ public class MKVHeaderBlock extends MKVHeader {
         return obj;
     }
 
-    /**
-     * <p>
-     * タグ本体に別のタグを含められるかどうかを返します。
-     * </p>
-     *
-     * @return タグ本体に別のタグを含められる場合は true、
-     * 含められない場合は false
-     */
     @Override
     public boolean isRecursive() {
         return false;
