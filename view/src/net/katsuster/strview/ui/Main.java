60ea5914b3154f2c8df15d85c80a0e7216ab21e0
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2017-02-15 22:39:43 +0900

    Remove debug codes.

--- view/src/net/katsuster/strview/ui/Main.java
+++ view/src/net/katsuster/strview/ui/Main.java
@@ -1,17 +1,10 @@
 package net.katsuster.strview.ui;
 
-import java.util.*;
-import java.io.*;
-import java.awt.HeadlessException;
+import java.awt.*;
 
 import javax.swing.*;
 
-import net.katsuster.strview.util.*;
-import net.katsuster.strview.io.*;
 import net.katsuster.strview.gui.*;
-import net.katsuster.strview.media.ts.*;
-import net.katsuster.strview.media.mkv.*;
-import net.katsuster.strview.media.m2v.*;
 
 /**
  * メインクラス。
@@ -22,35 +15,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        FileDropWindow w = null;
-
-        if (args.length > 0) {
-            String fname = args[0];
-            List<File> flist = new ArrayList<File>();
-
-            flist.add(new File(fname));
-
-            LargeBitList blist = new ByteToBitList(new FileByteList(fname));
-
-            /*TSPacketList tslist = new TSPacketList(blist);
-            tslist.count();
-            for (TSPacket a : tslist) {
-                System.out.println(a);
-            }*/
-
-            /*MKVTagList mkvlist = new MKVTagList(blist);
-            //mkvlist.count();
-            for (MKVTag a : mkvlist) {
-                System.out.println(a);
-            }*/
-
-            /*M2VDataList m2vlist = new M2VDataList(blist);
-            //m2vlist.count();
-            for (M2VData a : m2vlist) {
-                System.out.println(a);
-            }*/
-        }
-
         try {
             //Look and Feel をシステム標準に変更する
             try {
@@ -61,7 +25,7 @@ public class Main {
             }
 
             //結果表示用のフレームを作成する
-            w = new FileDropWindow();
+            FileDropWindow w = new FileDropWindow();
             w.setTitle("strview");
             w.setSize(300, 100);
             w.setVisible(true);
