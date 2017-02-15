54f4c0ef64eab8594d36ec6f6269522b4f76d5dc
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2017-02-15 22:36:11 +0900

    Enable to choose font of BinaryViewer.

--- view/src/net/katsuster/strview/gui/BinaryViewerWindow.java
+++ view/src/net/katsuster/strview/gui/BinaryViewerWindow.java
@@ -34,12 +34,19 @@ public class BinaryViewerWindow extends JFrame {
         JMenuBar topMenuBar = new JMenuBar();
         JMenu menuFile = new JMenu("ファイル(F)");
         menuFile.setMnemonic('f');
+        JMenu menuSetting = new JMenu("設定(S)");
+        menuSetting.setMnemonic('s');
 
         Action actionClose = new MenuActionClose(this, "閉じる(C)");
         actionClose.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_C);
         menuFile.add(actionClose);
 
+        Action actionFont = new MenuActionFont(this, "フォント(F)...");
+        actionFont.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_F);
+        menuSetting.add(actionFont);
+
         topMenuBar.add(menuFile);
+        topMenuBar.add(menuSetting);
         setJMenuBar(topMenuBar);
 
         //バイナリビューア
@@ -50,5 +57,31 @@ public class BinaryViewerWindow extends JFrame {
 
     public String getFilename() {
         return filename;
+    }
+
+    public class MenuActionFont extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+        private JFrame parent;
+
+        public MenuActionFont(JFrame f, String name) {
+            super(name);
+            parent = f;
+        }
+
+        public MenuActionFont(JFrame f, String name, Icon icon) {
+            super(name, icon);
+            parent = f;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JFontChooser chooser = new JFontChooser();
+
+            chooser.setSelectedFont(binaryViewer.getFont());
+            int res = chooser.showDialog(parent);
+            if (res == JFontChooser.OK_OPTION) {
+                binaryViewer.setFont(chooser.getSelectedFont());
+            }
+        }
     }
 }
