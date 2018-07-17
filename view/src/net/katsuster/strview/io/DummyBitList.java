2a9e9b22f5522dbe205ea23cfef13b2625710233
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2018-07-18 00:51:24 +0900

    Add dummy large bit list.

---
+++ view/src/net/katsuster/strview/io/DummyBitList.java
@@ -0,0 +1,63 @@
+package net.katsuster.strview.io;
+
+import net.katsuster.strview.util.*;
+
+/**
+ * <p>
+ * 何もしない LargeList クラスです。
+ * </p>
+ */
+public class DummyBitList extends AbstractLargeBitList
+        implements Cloneable {
+    /**
+     * <p>
+     * 指定された名前、長さ 0 のリストを作成します。
+     * </p>
+     *
+     * @param name リストの名前
+     */
+    public DummyBitList(String name) {
+        super(name, 0);
+    }
+
+    /**
+     * <p>
+     * 名前無し、指定された長さのリストを作成します。
+     * </p>
+     *
+     * @param l リストの長さ
+     */
+    public DummyBitList(long l) {
+        super(null, l);
+    }
+
+    /**
+     * <p>
+     * 指定された長さ、名前のリストを作成します。
+     * </p>
+     *
+     * @param name リストの名前
+     * @param l リストの長さ
+     */
+    public DummyBitList(String name, long l) {
+        super(name, l);
+    }
+
+    @Override
+    public DummyBitList clone()
+            throws CloneNotSupportedException {
+        DummyBitList obj = (DummyBitList)super.clone();
+
+        return obj;
+    }
+
+    @Override
+    public Boolean getInner(long index) {
+        return null;
+    }
+
+    @Override
+    public void setInner(long index, Boolean data) {
+        //do nothing
+    }
+}
